af94f7f121b637e47a1441ed1172681c52f24b6f
Gahstrio <ozankeremtelkok@gmail.com>
2020-02-08 17:40:40 +0300

    Setting up continuous integration

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,23 @@
 package com.mycompany.app;
 import java.util.ArrayList;
 public class App{
-    public static boolean search(ArrayList<Integer> array, int e){
-	System.out.println("inside search");
-	if (array == null) return false;
-	for (int elt: array){
-		if (elt == e) return true;
-	}
-	return false;
-}
+    public static boolean search(ArrayList<Integer> array1, ArrayList<Integer> array2, int start, int end){
+        if (array2 == null || array1 == null){
+            return false;
+        }
+        if (end>array1.size() || end>array2.size()){
+            return false;
+        }
+	for (int i=0; i<end; i++){
+            if (i>=start && i<=end){
+                if (array1.get(i)!=array2.get(i)){
+                    return false;
+                }
+              
+            }
+        }
+        return true;
+    }   
 }
 
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,23 +36,32 @@ public class AppTest
     {
         assertTrue( true );
     }
-       public void testFound() {
+       public void testTrue() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertTrue(new App().search(array, 4));
+      ArrayList<Integer> array2 =  new ArrayList<>(Arrays.asList(1,2,3,4));
+      assertTrue(new App().search(array,array2,0,3));
     }
 
-    public void testNotFound() {
+    public void testFalse() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertFalse(new App().search(array, 5));
+      ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1,1,1,1));
+      assertFalse(new App().search(array,array2,0,3));
     }
 
     public void testEmptyArray() {
       ArrayList<Integer> array = new ArrayList<>();
-      assertFalse(new App().search(array, 1));
+      ArrayList<Integer> array2  = new ArrayList<>();
+      assertFalse(new App().search(array,array2,1,2));
     }
-
+   
     public void testNull() {
-      assertFalse(new App().search(null, 1));
+      assertFalse(new App().search(null, null,1,2));
+    }
+    public void testWrongSize(){
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1,2,3,4));
+        assertFalse(new App().search(array1,array2,1,5));
+        
     }
 
 }
