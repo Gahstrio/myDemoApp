399fc4cd08bc7b9075849790e33365f6e1c79ba1
Gahstrio <ozankeremtelkok@gmail.com>
2020-02-08 18:53:39 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,23 +1,73 @@
 package com.mycompany.app;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.util.ArrayList;
-public class App{
-    public static boolean search(ArrayList<Integer> array1, ArrayList<Integer> array2, int start, int end){
-        if (array2 == null || array1 == null){
-            return false;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+public class App
+{
+    public static boolean search(ArrayList<Integer> array, int e) {
+      System.out.println("inside search");
+      if (array == null) return false;
+
+      for (int elt : array) {
+        if (elt == e) return true;
+      }
+      return false;
+    }
+public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          System.out.println(inputList);
+
+
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          int input2AsInt = Integer.parseInt(input2);
+
+          boolean result = App.search(inputList, input2AsInt);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        if (end>array1.size() || end>array2.size()){
-            return false;
-        }
-	for (int i=0; i<end; i++){
-            if (i>=start && i<=end){
-                if (array1.get(i)!=array2.get(i)){
-                    return false;
-                }
-              
-            }
-        }
-        return true;
-    }   
+       return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
-
-
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -38,29 +38,21 @@ public class AppTest
     }
        public void testTrue() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      ArrayList<Integer> array2 =  new ArrayList<>(Arrays.asList(1,2,3,4));
-      assertTrue(new App().search(array,array2,0,3));
+      assertTrue(new App().search(array,3));
     }
 
-    public void testFalse() {
-      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1,1,1,1));
-      assertFalse(new App().search(array,array2,0,3));
-    }
 
     public void testEmptyArray() {
       ArrayList<Integer> array = new ArrayList<>();
-      ArrayList<Integer> array2  = new ArrayList<>();
-      assertFalse(new App().search(array,array2,1,2));
+      assertFalse(new App().search(array,2));
     }
    
     public void testNull() {
-      assertFalse(new App().search(null, null,1,2));
+      assertFalse(new App().search(null,2));
     }
     public void testWrongSize(){
         ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
-        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(1,2,3,4));
-        assertFalse(new App().search(array1,array2,1,5));
+        assertFalse(new App().search(array1,10));
         
     }
 
